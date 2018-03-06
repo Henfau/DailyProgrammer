2a418a0d100961afe05cd40c5dee2241cdd4fd7e
Henfau <henrik.fauskanger@gmail.com>
2018-03-06 23:04:57 +0100

    add script to convert base10 numbers into base62

--- imgurlinks/main.cpp
+++ imgurlinks/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    /*
+     *
+     * https://www.reddit.com/r/dailyprogrammer/comments/7yyt8e/20180220_challenge_352_easy_making_imgurstyle/
+     *
+     */
+
+    char base62[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    long long int input = 237860461;
+
+    int digits = 0;
+
+    for(int i = 0;pow(62,i)<input;i++){
+        digits = i;
+    }
+    char output[digits];
+
+    long long int remainder;
+    long long int digit;
+    for(int i = digits;i>=0;i--){
+        digit = input/ static_cast<long long int>(pow(62,i));
+        remainder = input% static_cast<long long int>(pow(62,i));
+        input = remainder;
+        output[digits-i]= base62[digit];
+        std::cout << output[digits-i];
+    }
     return 0;
 }
 
